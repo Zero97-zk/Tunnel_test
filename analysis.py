36aa169b5b9562188632060119f71b8375bd5ac4
Zerozk-97 <zk_zero_97@163.com>
2020-07-17 17:37:34 +0800

    [IMP]analysis.py

--- analysis.py
+++ analysis.py
@@ -11,43 +11,98 @@ def get_analysis(db):
         # print(supplier)
         total_count = supplier[1]
 
-        # 获取有效IP数
+        # 获取有效请求数
         cursor = db.execute("SELECT count(*) from `tunnel_info1` where supplier='{}' and is_ok=1;".format(
             supplier[0],
         ))
-        is_ok_count = cursor.fetchone()[0]
-
-        # 获取有效IP平均响应速度
-        cursor = db.execute("SELECT AVG(resp_speed) from `tunnel_info1` where supplier='{}' and is_ok=1;".format(
-            supplier[0],
-        ))
-        avg_resp_speed = cursor.fetchone()[0]
+        ip_ok_count = cursor.fetchone()[0]
+        ip_ok_rate = ip_ok_count / total_count
 
         # 获取优质响应数 响应时间小于1秒的
         cursor = db.execute("SELECT count(*) from `tunnel_info1` where supplier='{}' and is_ok=1 and resp_speed<1;".format(
             supplier[0],
         ))
-        good_resp_count = cursor.fetchone()[0]
+        resp_lt_1_count = cursor.fetchone()[0]
+        resp_lt_1_rate =resp_lt_1_count / ip_ok_count
 
-        # 获取响应速度方差
+        # 获取响应时间小于2秒的响应
+        cursor = db.execute(
+            "SELECT count(*) from `tunnel_info1` where supplier='{}' and is_ok=1 and resp_speed<1;".format(
+                supplier[0],
+            ))
+        resp_lt_2_count = cursor.fetchone()[0]
+        resp_lt_2_rate = resp_lt_2_count / ip_ok_count
+
+
+        # 从小于2秒的响应中获取平均响应速度
+        cursor = db.execute(
+            "SELECT AVG(resp_speed) from `tunnel_info1` where supplier='{}' and is_ok=1 and resp_speed<2;".format(
+                supplier[0],
+            ))
+        avg_resp_speed = cursor.fetchone()[0]
+
+        # 从小于2秒的响应中获取响应速度方差
         cursor = db.execute("SELECT VARIANCE(resp_speed) from `tunnel_info1` where supplier='{}' and is_ok=1;".format(
             supplier[0],
         ))
         var_resp_speed = cursor.fetchone()[0]
 
         # 获取IP city分布
+        cursor = db.execute(
+            "SELECT count(DISTINCT prov) from `tunnel_info1` where supplier='{}' and is_ok=1;".format(
+                supplier[0],
+            ))
+        ip_prov_count = cursor.fetchone()[0]
+
+        # 获取IP city分布
         cursor = db.execute("SELECT count(DISTINCT city) from `tunnel_info1` where supplier='{}' and is_ok=1;".format(
             supplier[0],
         ))
-        ip_dist_count = cursor.fetchone()[0]
+        ip_city_count = cursor.fetchone()[0]
 
-        # 获取IP 重复率
-        cursor = db.execute("SELECT count(DISTINCT proxy_ip), count(*) from `tunnel_info1` where supplier='{}' and is_ok=1;".format(
+        # 获取IP总量
+        cursor = db.execute("SELECT count(DISTINCT proxy_ip) from `tunnel_info1` where supplier='{}' and is_ok=1;".format(
             supplier[0],
         ))
+        ip_total_count = cursor.fetchone()[0]
+        # ip重复率
+        ip_repetitive_rate = ip_total_count / ip_ok_count
+
+        cursor = db.execute(
+            "SELECT count(DISTINCT proxy_ip),count(proxy_ip) from `tunnel_info1` where supplier='{}' and is_ok=1 and created_time>='2020-07-17 00:00:00' and created_time<'2020-07-17 00:01:00';".format(
+                supplier[0],
+            ))
+        tmp =  cursor.fetchone()
+        ip_1_mi_count, ip_1_mi_total_count = tmp[0], tmp[1]
+        # ip1分钟重复率
+        ip_repetitive_rate_1_mi = ip_1_mi_count / ip_1_mi_total_count
+
+        cursor = db.execute(
+            "SELECT count(DISTINCT proxy_ip),count(proxy_ip) from `tunnel_info1` where supplier='{}' and is_ok=1 and created_time>='2020-07-17 00:00:00' and created_time<'2020-07-17 00:05:00';".format(
+                supplier[0],
+            ))
         tmp = cursor.fetchone()
-        # ip_dist_rate = cursor.fetchone()[0]/cursor.fetchone()[1]
-        ip_dist_rate = (tmp[1]-tmp[0])/tmp[1]
+        ip_5_mi_count, ip_5_mi_total_count = tmp[0], tmp[1]
+        # ip5分钟重复率
+        ip_repetitive_rate_5_mi = ip_5_mi_count / ip_5_mi_total_count
+
+        cursor = db.execute(
+            "SELECT count(DISTINCT proxy_ip),count(proxy_ip) from `tunnel_info1` where supplier='{}' and is_ok=1 and created_time>='2020-07-17 00:00:00' and created_time<'2020-07-17 01:00:00';".format(
+                supplier[0],
+            ))
+        tmp = cursor.fetchone()
+        ip_1_h_count, ip_1_h_total_count = tmp[0], tmp[1]
+        # ip1小时重复率
+        ip_repetitive_rate_1_h = ip_1_h_count / ip_1_h_total_count
+
+        cursor = db.execute(
+            "SELECT count(DISTINCT proxy_ip),count(proxy_ip) from `tunnel_info1` where supplier='{}' and is_ok=1 and created_time>='2020-07-15 00:00:00' and created_time<'2020-07-16 00:00:00';".format(
+                supplier[0],
+            ))
+        tmp = cursor.fetchone()
+        ip_1_d_count, ip_1_d_total_count = tmp[0], tmp[1]
+        # ip1小时重复率
+        ip_repetitive_rate_1_d = ip_1_d_count / ip_1_d_total_count
 
         # 求各代理商带宽情况
         ## 获取带宽平均值
@@ -55,34 +110,45 @@ def get_analysis(db):
             "SELECT AVG(bandwidth) from `tunnel_info2` where supplier='{}' and is_ok=1;".format(
                 supplier[0],
             ))
-        avg_band_width = cursor.fetchone()[0]
+        avg_bandwidth = cursor.fetchone()[0]
+        avg_bandwidth = avg_bandwidth / 1024 if avg_bandwidth else None
 
-        ## 获取带宽方差
-        # cursor = db.execute("SELECT VARIANCE(bandwidth) from `tunnel_info2` where supplier='{}' and is_ok=1;".format(
-        #     supplier[0],
-        # ))
-        # var_band_width = cursor.fetchone()[0]
+        cursor = db.execute(
+            "SELECT count(*) from `tunnel_info2` where supplier='{}';".format(
+                supplier[0],
+            ))
+        band_total_count = cursor.fetchone()[0]
+
+        cursor = db.execute(
+            "SELECT count(*) from `tunnel_info2` where supplier='{}and is_ok=1';".format(
+                supplier[0],
+            ))
+        band_ok_count = cursor.fetchone()[0]
+
+        band_ok_rate = band_ok_count / band_total_count
 
         tmp = {
-            "name": supplier[0],
-            "total_count": total_count,
-            "is_ok_count": is_ok_count,
-            "avg_resp_speed": "%.2fs"%(avg_resp_speed),
-            "good_resp_count": good_resp_count,
-            "var_resp_speed": var_resp_speed,
-            "ip_dist_count": ip_dist_count,
-            "ip_dist_rate": ip_dist_rate,
-            "avg_band_width": "%.2fk" % (
-                        avg_band_width / 1024) if avg_band_width else None,
-            # "var_band_width": var_band_width/(1024**2) if var_band_width else None,
-
+            "supplier": supplier[0],                            # 供应商
+            "total_count": total_count,                         # 请求总量
+            "ip_ok_rate": ip_ok_rate,                           # 响应成功率
+            "avg_band_width": "%.2fk" % (avg_bandwidth),        # 带宽
+            "band_ok_rate": "%.2f " % band_ok_rate,             # 带宽响应成功率
+            "resp_lt_1_rate": resp_lt_1_rate,                   # 响应时长小于1秒占比
+            "resp_lt_2_rate": resp_lt_2_rate,                   # 响应时长小于2秒占比
+            "avg_resp_speed": "%.2fs"%(avg_resp_speed),         # 平均响应时长
+            "var_resp_speed": var_resp_speed,                   # 响应时长方差
+            "ip_total_count": ip_total_count,                   # ip总量
+            "ip_repetitive_rate": ip_repetitive_rate,           # ip重复率
+            "ip_repetitive_rate_1_mi": ip_repetitive_rate_1_mi,           # ip重复率
+            "ip_repetitive_rate_5_mi": ip_repetitive_rate_5_mi,           # ip重复率
+            "ip_repetitive_rate_1_h": ip_repetitive_rate_1_h,           # ip重复率
+            "ip_repetitive_rate_1_d": ip_repetitive_rate_1_d,           # ip重复率
+            "ip_prov_count": ip_prov_count,                     # ip涉及的省数量
+            "ip_city_count": ip_city_count,                     # ip涉及的市数量
         }
+
         # print(tmp)
         ret.append(tmp)
-
-
-
-        # print(supplier[0], total_count, is_ok_count, avg_resp_speed, good_resp_count, var_resp_speed, ip_dist_count, ip_dist_rate)
     return ret
 
 def trans2table(data):
@@ -91,12 +157,10 @@ def trans2table(data):
     tb.field_names = [c for c in data[0]]
     for c in data:
         tb.add_row([x for x in c.values()])
-
     print(tb)
 
 
 if __name__ == "__main__":
     db = get_db_local()
-
     data = get_analysis(db)
     trans2table(data)
